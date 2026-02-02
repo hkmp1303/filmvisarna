028a5f5ece86fd0afc48fc64185addde1ddbb4fe
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-02 15:10:25 +0100

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import type { RouteObject } from 'react-router'
+import { createBrowserRouter, RouterProvider, } from 'react-router'
+import routes from './routes.tsx'
+
+
 import './index.css'
 import App from './App.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: routes as RouteObject[]
+  }
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
