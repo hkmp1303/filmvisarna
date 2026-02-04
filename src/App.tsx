7071c207a5c043f69b194c2e512a1d9ab00965e8
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-04 09:23:48 +0100

    Added header to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import { useState } from 'react'
 import Main from './partials/Main'
+import Header from './partials/Header'
 
 
 import './App.css'
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-
+      <Header />
       <Main />
 
     </>
