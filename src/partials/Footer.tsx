839d1b5f35284cc32954a8d61b877d7679f2b5a2
OscarK99Swe <oscar.kock99@gmail.com>
2026-02-06 10:49:33 +0100

    Added DynData
    Added DynData to our project

---
+++ src/partials/Footer.tsx
@@ -0,0 +1,17 @@
+
+/* 
+ Option 1: Antingen upptäck, men mest troligast AI chatbot
+ Option 2: Login/Your account (Link to my account page)
+ Option 3: Menu bar -> Links to "Tema Dagar", "Om oss", "Kontakt", "kiosk" -> 
+ -> eventuellt fler/mindre alternativ 
+ */
+
+
+export default function Footer() {
+  return <footer>
+
+
+
+
+  </footer>;
+}
