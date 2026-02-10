af9f2bdb1244fd01cc5a82480c86a5032bdf096b
pyr0xd <kalkestatimoty@gmail.com>
2026-02-10 22:02:23 +0100

    Added a few more rows and buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,24 @@
 const LoginGallery: React.FC = () => {
   return (
     <div className="login-container">
-      <h1>Login</h1>
+      <div className="login-big-text">
+        <h1>Login</h1>
+      </div>
+      <div className="login-input">
+        <div className="email-input">
+          <input type="txet" placeholder="gmail"></input>
+        </div>
+        <div className="Password-input">
+          <input type="txet" placeholder="lösenord"></input>
+        </div>
+        <div className="forgotten-password">
+          <button className="forgoten-password-btn">glömt lösenord</button>
+        </div>
+        <div className="confirm- button"></div>
+
+      </div>
     </div>
+
   );
 };
 
