04d0bc2b67af53d80173c2a6c4ced43d5c09f89e
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-10 13:07:47 +0100

    started writing css AND added a movie trailer!!
    I'm so happy I got the youtube trailer to work! you had to delete som of
    thier embedded code to get it to work!

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../css/MovieDetails.css';
 
 export default function MovieDetails() {
 
@@ -6,38 +7,43 @@ export default function MovieDetails() {
     const [date, setDate] = useState('');
 
     return <>
-        <article>
+        <article className="movie-details-container">
             <section className="movie-title">
                 <h1>Title of the movie</h1>
                 <h2>Åldersgräns: xxx länged: xxx Genre: xxx</h2>
             </section>
-            <video src=""></video>
-            <div>
-                <label htmlFor="start">Välj datum: </label>
-                <input
-                    type="date"
-                    id="start"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                />
-                <p>Valt datum: {date}</p>
+            <div className="colum1-container">
+                <video src=""></video>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZDlYxy69R3A?si=mSlEj8VkAXaEjGk4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"   ></iframe>
+                <div>
+                    <label htmlFor="start">Välj datum: </label>
+                    <input
+                        type="date"
+                        id="start"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                    <p>Valt datum: {date}</p>
+                </div>
+                <button>Boka biljetter</button>
             </div>
-            <button>Boka biljetter</button>
-            <section className="movie-description">
-                <p>long text about the movie and what it is about</p>
-                <p>long text about the movie and what it is about</p>
-                <p>long text about the movie and what it is about</p>
-                <p>long text about the movie and what it is about</p>
-                <p>long text about the movie and what it is about</p>
-            </section>
-            <section className="movie-cast">
-                <h3>Cast</h3>
-                <p>some actor</p>
-                <p>some actor</p>
-                <p>some actor</p>
-                <p>some actor</p>
-                <p>some actor</p>
-            </section>
+            <div className="colum2-container">
+                <section className="movie-description">
+                    <p>long text about the movie and what it is about</p>
+                    <p>long text about the movie and what it is about</p>
+                    <p>long text about the movie and what it is about</p>
+                    <p>long text about the movie and what it is about</p>
+                    <p>long text about the movie and what it is about</p>
+                </section>
+                <section className="movie-cast">
+                    <h3>Cast</h3>
+                    <p>some actor</p>
+                    <p>some actor</p>
+                    <p>some actor</p>
+                    <p>some actor</p>
+                    <p>some actor</p>
+                </section>
+            </div>
         </article>
     </>
 }
