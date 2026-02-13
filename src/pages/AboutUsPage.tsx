478e86f0d8b583e6176b1eb985a75213cc23410d
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-13 13:28:59 +0100

    made basic html and creates css file

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,8 +1,18 @@
-
+import '../css/AboutUsPage.css'
 
 export default function AboutUS() {
 
     return <>
+        <article className="aboutus-page">
+            <section className="aboutus-info">
 
+            </section>
+            <div className="big-logo">
+
+            </div>
+            <section className="side-link">
+
+            </section>
+        </article>
     </>
 }
