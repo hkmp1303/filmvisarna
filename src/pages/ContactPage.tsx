51131aae9a68480583fc62611bcc3319566a2272
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-16 15:20:07 +0100

    Made the basics for the contact page
    made mock data
    made the form
    made basic css

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,24 +1,50 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
 import '../css/ContactPage.css'
 
-const [data, setData] = useState();
+//const [name, setName] = useState();
+//const [email, setEmail] = useState();
+//const [question, setQuestion] = useState();
+//const [message, setMessage] = useState();
 
 export default function Contact() {
 
-    return <article>
-        <form action=""></form>
-        <label> Namn
-            <input type="text" />
-        </label>
-        <select name="" id="">
-            <option value="">Välja ämne som det handlar om</option>
-            <option value="">ämne 1</option>
-            <option value="">ämne 2</option>
-            <option value="">ämne 3</option>
-        </select>
-        <label htmlFor="">Skriv
-            <textarea name="" id=""></textarea>
-        </label>
+    return <article className='contact-page'>
+        <section className='info-container'>
+            <h1>Kontakta oss</h1>
+            <h3>Adress:</h3>
+            <p>Storgatan 12</p>
+            <p>123 45 Filmstad</p>
+            <p>Sverige</p>
+            <h3>Telefon:</h3>
+            <p>01-000000000</p>
+            <h3>E-mail:</h3>
+            <p>info@filmvisarna.fake</p>
+            <h3>Öppettider:</h3>
+            <p>Måndag - Fredag: 13:00 - 23:00</p>
+            <p>Lördag - Söndag: 10:00 - 00:00</p>
+
+        </section>
+        <section className='form-container'>
+            <div className='form-box'>
+                <form action=""></form>
+                <label> Namn: </label>
+                <input className='input-field' type="text" />
+                <label> E-mail: </label>
+                <input className='input-field' type="text" />
+                <select name="" id="">
+                    <option value="">Ärende</option>
+                    <option value="">Föreställning</option>
+                    <option value="">Biljettfråga</option>
+                    <option value="">Kiosken</option>
+                    <option value="">Betalning</option>
+                    <option value="">Övrigt</option>
+                </select>
+                <label htmlFor="">Skriv
+                </label>
+                <textarea></textarea>
+                <input type="submit" className='submit-btn' />
+            </div>
+        </section>
     </article>
 }
