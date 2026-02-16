370a06882c4a071209adb42a5e6d109323f9b977
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-16 12:50:14 +0100

    fixed mobile view

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -45,7 +45,7 @@ export default function Footer() {
         {menuOpen && (
           <div className="footer-submenu">
             <Link to="/tema-dagar">Tema dagar</Link>
-            <Link to="/om-oss">Om oss</Link>
+            <Link to="/aboutus">Om oss</Link>
             <Link to="/kontakt">Kontakt</Link>
             <Link to="/kiosk">Kiosk</Link>
           </div>
