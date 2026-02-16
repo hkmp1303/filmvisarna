2a0fa6da725347408e000d6336a68ccfd9126ddc
M-Renerg <mikael.k.renberg@hotmail.com>
2026-02-16 13:52:12 +0100

    basic structure

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react'
+
 import '../css/ContactPage.css'
+
+const [data, setData] = useState();
 
 export default function Contact() {
 
     return <article>
-
+        <form action=""></form>
+        <label> Namn
+            <input type="text" />
+        </label>
+        <select name="" id="">
+            <option value="">Välja ämne som det handlar om</option>
+            <option value="">ämne 1</option>
+            <option value="">ämne 2</option>
+            <option value="">ämne 3</option>
+        </select>
+        <label htmlFor="">Skriv
+            <textarea name="" id=""></textarea>
+        </label>
     </article>
 }
