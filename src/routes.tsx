f0c3ed3595c68b8745139258a124b08afac4958c
M-Renerg <mikael.k.renberg@hotmail.com>
2026-03-02 14:53:42 +0100

    VimCrypt~03!Êó´>·©Ãkõé!<;wgQ}@"{Ú0ªEÍ{`- Æ;º3¸Lf¥,ûÿz½Í{ÿ4û3Í/°àQvͶBO!·:SQ¦«@ßSïàaږÒ$MÆHèÕãhôôk䘜Axp¸íñØÄÕʹgîô?òt£ö¼5¨ü!ØlC÷÷IÃ˞Z;4«n½÷µ1©¬Iµu¤6%ÎêP|Òpþ6ª\oØÝвã¶vjKȓOɴìSZôâ@M_ã¶Ý9Ä1ÏÊãí>F2a^_ÜCö¹Ñf½Àeº_ɩ\Á術ósƨ+­åõ<aʛ]3ΜO}!Zýò¸ëA<WÎMRz-riî®(0UEb-íò0ÉK£|~Èèæ̟îx÷äoi¢â*ÙE¼ùaÈU åE

--- src/routes.tsx
+++ src/routes.tsx
@@ -21,7 +21,7 @@ const routes: Route[] = [
   { element: <MovieDetails />, path: '/moviedetails/:filmid' },
   { element: <Booking />, path: '/booking' },
   { element: <Kiosk />, path: '/Kiosk', menuLabel: 'Kiosk' },
-  { element: <Register />, path: '/Register', menuLabel: 'Registrera' }
+  { element: <Register />, path: '/Register' }
 
   //{ element: <Page-Name />, path: '/file-path', menuLabel: 'Lable-Name' }
 ];
