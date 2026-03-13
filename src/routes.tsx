088bbc5d3c0a23b3814cd3eece4e1b2c4c474862
OscarK99Swe <oscar.kock99@gmail.com>
2026-03-13 11:30:09 +0100

    proper routes
    added routing under login to help the illusion of swapping login and profile in header

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import type { JSX } from 'react';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import Profile from './pages/ProfilePage';
 import MovieDetails from './pages/MovieDetailsPage';
 import AboutUs from './pages/AboutUsPage';
 import Contact from './pages/ContactPage';
@@ -11,7 +12,7 @@ import TemaDagar from './pages/TemaDagar';
 import ConfirmBooking from './pages/ConfirmBooking';
 import PasswordRecovery from './pages/PasswordRecoveryPage';
 import ResetPassword from './pages/ResetPasswordPage';
-import Profile from './pages/ProfilePage';
+
 
 interface Route {
   element: JSX.Element;
@@ -22,6 +23,7 @@ interface Route {
 const routes: Route[] = [
   { element: <LandingPage />, path: '/', menuLabel: 'Hem' },
   { element: <Login />, path: '/login', menuLabel: 'Login' },
+  { element: <Profile />, path: '/profile', menuLabel: 'Profil' },
   { element: <AboutUs />, path: '/aboutus', menuLabel: 'Om oss' },
   { element: <Contact />, path: '/contact', menuLabel: 'Kontakt' },
   { element: <MovieDetails />, path: '/moviedetails/:filmid' },
@@ -31,8 +33,7 @@ const routes: Route[] = [
   { element: <TemaDagar />, path: '/themedays', menuLabel: 'Temadagar' },
   { element: <ConfirmBooking />, path: '/confirmbooking' },
   { element: <PasswordRecovery />, path: '/passwordrecovery' },
-  { element: <ResetPassword />, path: '/reset-password' },
-  { element: <Profile />, path: '/profile', menuLabel: 'Profil' }
+  { element: <ResetPassword />, path: '/reset-password' }
 
 
   //{ element: <Page-Name />, path: '/file-path', menuLabel: 'Lable-Name' }
