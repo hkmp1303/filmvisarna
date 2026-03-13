3cf960a5c33117fa6fd4d8216d2fdceedc57a4be
M-Renerg <mikael.k.renberg@hotmail.com>
2026-03-13 14:42:01 +0100

    funny loading thingy

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -26,7 +26,32 @@ export default function LandingPage() {
   };
 
   if (!movies) {
-    return <div style={{ color: 'white' }}>Laddar filmer...</div>;
+    return <div className="flex flex-col items-center justify-center min-h-[50vh] w-full">
+      <svg
+        className="animate-spin h-10 w-10 text-purple-500 mb-4"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <p className='text-white text-lg font-semibold animate-pulse'>
+        Laddar filmer...
+      </p>
+    </div>
+      ;
   }
   const displayMovies = sortAndFilterMovies(movies, sortBy, searchQuery, selectedGenre);
 
@@ -88,7 +113,7 @@ export default function LandingPage() {
             <div className="poster-container">
               <div
                 className="poster-placeholder"
-                style={{ backgroundImage: `url(${movie.cover_image??''})` }}
+                style={{ backgroundImage: `url(${movie.cover_image ?? ''})` }}
               >
                 <div className="poster-overlay-text">
                   <h3>{movie.title}</h3>
