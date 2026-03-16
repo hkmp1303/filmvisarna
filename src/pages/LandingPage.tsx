92f977e048c42d80ac95d711dcd7f1c76fa765f5
M-Renerg <mikael.k.renberg@hotmail.com>
2026-03-16 10:18:11 +0100

    refactored the code and made it into functions

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,7 +8,7 @@ import type { BriefFilm } from '../utilities/filmInterface';
 import '../css/LandingPage.css';
 import translateGenre from '../utilities/i18n';
 import { displayVeiwerRating } from '../utilities/i18n';
-
+import { LoadingWheel } from '../utilities/loadingWheel';
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -26,32 +26,8 @@ export default function LandingPage() {
   };
 
   if (!movies) {
-    return <div className="flex flex-col items-center justify-center min-h-[50vh] w-full">
-      <svg
-        className="animate-spin h-10 w-10 text-purple-500 mb-4"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-      <p className='text-white text-lg font-semibold animate-pulse'>
-        Laddar filmer...
-      </p>
-    </div>
-      ;
+    return LoadingWheel();
+
   }
   const displayMovies = sortAndFilterMovies(movies, sortBy, searchQuery, selectedGenre);
 
