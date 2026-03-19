c68425d51c039cc252bedae1bc3c82ab45752263
Heather <101600223+hkmp1303@users.noreply.github.com>
2026-03-19 12:50:47 +0100

    Revert "Booking reservations"

--- src/utilities/bookingInterface.ts
+++ src/utilities/bookingInterface.ts
@@ -4,14 +4,3 @@ export interface Booking {
   showtime: string;
   poster: string;
 }
-
-export interface BookingFull {
-  bookingid: number;
-  total_cost: number;
-  date: string;
-  guid: string;
-  status: 'reserved' | 'booked' | 'canceled' | string;
-  screeningid: number;
-  userid: number;
-  error: string;
-}
